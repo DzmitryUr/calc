85e6148ead9f8965752fcdd09b1191450ca407ce
DzmitryUr <dimaau1@gmail.com>
2018-12-11 11:01:01 +0300

    'init'

--- src/App.js
+++ src/App.js
@@ -1,27 +1,49 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <Digit num="0" id="zero"/>
+        <Digit num="1" id="one"/>
+        <Digit num="2" id="two"/>
+        <Digit num="3" id="three"/>
+        <Digit num="4" id="four"/>
+        <Digit num="5" id="five"/>
+        <Digit num="6" id="six"/>
+        <Digit num="7" id="seven"/>
+        <Digit num="8" id="eight"/>
+        <Digit num="9" id="nine"/>
+        <MathOperator num="+" id="add"/>
+        <MathOperator num="-" id="subtract"/>
+        <MathOperator num="*" id="multiply"/>
+        <MathOperator num="/" id="divide"/>
+        <button id="equals">=</button>        
       </div>
     );
+  }
+}
+
+class Digit extends Component {
+  constructor(props) {
+    super(props);
+  }
+  render() {
+    return (
+      <button id={this.props.id}>{this.props.num}</button>
+    )
+  }
+}
+
+class MathOperator extends Component {
+  constructor(props) {
+    super(props);
+  }
+  render() {
+    return (
+      <button id={this.props.id}>{this.props.num}</button>
+    )
   }
 }
 
