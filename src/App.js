3f433f9ae36b645d1030895d58042eae7ac0f0a3
DzmitryUr <dimaau1@gmail.com>
2018-12-12 10:58:36 +0300

    add SASS preprocessor, change styles

--- src/App.js
+++ src/App.js
@@ -1,25 +1,31 @@
 import React, { Component } from 'react';
-import './App.css';
+import './App.scss';
+import Display from './compenents/display'
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <Digit num="0" id="zero"/>
-        <Digit num="1" id="one"/>
-        <Digit num="2" id="two"/>
-        <Digit num="3" id="three"/>
-        <Digit num="4" id="four"/>
-        <Digit num="5" id="five"/>
-        <Digit num="6" id="six"/>
-        <Digit num="7" id="seven"/>
-        <Digit num="8" id="eight"/>
-        <Digit num="9" id="nine"/>
-        <MathOperator num="+" id="add"/>
-        <MathOperator num="-" id="subtract"/>
-        <MathOperator num="*" id="multiply"/>
-        <MathOperator num="/" id="divide"/>
-        <button id="equals">=</button>        
+        <div className="calc-container">
+          <Digit num="0" id="zero"/>
+          <Digit num="1" id="one"/>
+          <Digit num="2" id="two"/>
+          <Digit num="3" id="three"/>
+          <Digit num="4" id="four"/>
+          <Digit num="5" id="five"/>
+          <Digit num="6" id="six"/>
+          <Digit num="7" id="seven"/>
+          <Digit num="8" id="eight"/>
+          <Digit num="9" id="nine"/>
+          <MathOperator num="+" id="add"/>
+          <MathOperator num="-" id="subtract"/>
+          <MathOperator num="*" id="multiply"/>
+          <MathOperator num="/" id="divide"/>
+          <MathOperator num="." id="decimal"/>
+          <MathOperator num="AC" id="clear"/>
+          <button id="equals">=</button>     
+          <Display />   
+        </div>
       </div>
     );
   }
@@ -37,9 +43,6 @@ class Digit extends Component {
 }
 
 class MathOperator extends Component {
-  constructor(props) {
-    super(props);
-  }
   render() {
     return (
       <button id={this.props.id}>{this.props.num}</button>
---
+++ src/compenents/display.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+class Display extends Component {
+    constructor(props) {
+      super(props);
+    }
+    render() {
+      return (
+        <div id="display">0</div>
+      )
+    }
+  }
+
+  export default Display
