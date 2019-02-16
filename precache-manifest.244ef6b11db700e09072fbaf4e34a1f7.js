b48a547a097ea8f9347064fa16c3cba822d38b5c
DzmitryUr <dimaau1@gmail.com>
2019-02-16 21:14:54 +0300

    Updates

---
+++ precache-manifest.244ef6b11db700e09072fbaf4e34a1f7.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "b5f5139b78fafdf93bd1",
+    "url": "/calculator/static/js/runtime~main.b5f5139b.js"
+  },
+  {
+    "revision": "54b976191cece867b8fd",
+    "url": "/calculator/static/js/main.54b97619.chunk.js"
+  },
+  {
+    "revision": "c60072b4973ca76cd2d6",
+    "url": "/calculator/static/js/1.c60072b4.chunk.js"
+  },
+  {
+    "revision": "54b976191cece867b8fd",
+    "url": "/calculator/static/css/main.be92af9e.chunk.css"
+  },
+  {
+    "revision": "91e4d44586865f33ffe73b6fdd587c16",
+    "url": "/calculator/index.html"
+  }
+];
